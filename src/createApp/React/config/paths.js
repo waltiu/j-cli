974ab3b3f65fae563bdbb6ecea73636930f13c39
waltiu <waltiu@163.com>
2022-07-14 18:27:08 +0800

    feat: 代码模板库从git上下载不存放在cli工程中

--- src/createApp/React/config/paths.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  public: path.resolve(__dirname, '../public'),
-  src: path.resolve(__dirname, '../src'),
-  build: path.resolve(__dirname, '../build')
-}
--- src/createApp/React/src/modules/components/index.js
+++
@@ -1,4 +0,0 @@
-export { TodoForm } from './TodoForm'
-export { TodoList } from './TodoList'
-export { TodoFilters } from './TodoFilters'
-export { TodoControls } from './TodoControls'
